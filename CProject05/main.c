1a7504550d115577aeb35b925e75fb6b6c16d0fb
elflee77 <elflee77@gmail.com>
2026-03-23 14:31:18 +0900

    .

---
+++ CProject05/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <windows.h>	//윈도우32 라이브러리
+
+int main()
+{
+	int iterator = 0;
+
+	//반복문 : while (조건) { 반복내용 }
+	while (iterator < 10)
+	{
+		system("cls");	//시스템 명령어를 사용한다 system(명령어 문자열)
+		printf("오늘 점심메뉴 알려줘 %d초 준다.\n", 10 - iterator);
+		iterator = iterator + 1;
+		Sleep(1000);	//ms 단위로 쉰다. : Sleep(ms)
+	}
+
+	return 0;
+}
--- CTestProject/main.c
+++ CTestProject/main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
-#include <windows.h>	//윈도우32 라이브러리
+
+int move_cursor(int x, int y)
+{
+	printf("\033[%d;%dH", y, x);	//printf : print function
+	return 0;
+}
 
 int main()
 {
-	int iterator = 0;
+	int x = 0;
+	int y = 0;
+	int isRunning = 1;
 
-	//반복문 : while (조건) { 반복내용 }
-	while (iterator < 10)
+	while (isRunning)
 	{
-		system("cls");	//시스템 명령어를 사용한다 system(명령어 문자열)
-		printf("오늘 점심메뉴 알려줘 %d초 준다.\n", 10 - iterator);
-		iterator = iterator + 1;
-		Sleep(1000);	//ms 단위로 쉰다. : Sleep(ms)
-	}	
-
+		char a = getch();
+		if (a == ' ')
+		{
+			x = x + 1;
+		}
+		if (a == 'q')
+		{
+			isRunning = 0;
+		}
+		system("cls");
+		move_cursor(x, y);
+		printf("*");
+	}
 	return 0;
 }
