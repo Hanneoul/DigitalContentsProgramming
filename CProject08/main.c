9349fe3cd85e274762b12e229803705198040781
elflee77 <elflee77@gmail.com>
2026-03-30 14:01:30 +0900

    .

---
+++ CProject08/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int move_cursor(int x, int y)
+{
+	printf("\033[%d;%dH", y, x);	//printf : print function
+	return 0;
+}
+
+int main()
+{
+	int x = 0;
+	int y = 0;
+	int isRunning = 1;
+
+	while (isRunning)
+	{
+		char a = getch();
+		if (a == ' ')
+		{
+			x = x + 1;
+		}
+		if (a == 'q')
+		{
+			isRunning = 0;
+		}
+		system("cls");
+		move_cursor(x, y);
+		printf("*");
+	}
+
+
+	return 0;
+}
--- CTestProject/main.c
+++ CTestProject/main.c
@@ -1,5 +1,69 @@
 #include <stdio.h>
 
+// 모든 서식을 기본값으로 되돌리는 매크로
+#define COLOR_RESET "\x1b[0m"
+
+#define FONT_COLOR_BLACK 30
+#define BG_COLOR_BLACK 40
+#define FONT_COLOR_RED 31
+#define BG_COLOR_RED 41
+#define FONT_COLOR_GREEN 32
+#define BG_COLOR_GREEN 42
+#define FONT_COLOR_BLUE 34
+#define BG_COLOR_BLUE 44
+#define FONT_COLOR_MAGENTA 35
+#define BG_COLOR_MAGENTA 45
+#define FONT_COLOR_BRIGHTMAGENTA 95
+#define BG_COLOR_BRIGHTMAGENTA 105
+#define FONT_COLOR_WHITE 37
+#define BG_COLOR_WHITE 47
+
+/*
+ * ANSI Escape Sequence 색상 코드 가이드 (16색 표준)
+ * ---------------------------------------------------------
+ * [폰트 색상 (FG)]      [배경 색상 (BG)]      [색상명]
+ * 30                  40                  Black
+ * 31                  41                  Red
+ * 32                  42                  Green
+ * 33                  43                  Yellow
+ * 34                  44                  Blue
+ * 35                  45                  Magenta
+ * 36                  46                  Cyan
+ * 37                  47                  White
+ * ---------------------------------------------------------
+ * [밝은 폰트 (FG)]      [밝은 배경 (BG)]      [색상명]
+ * 90                  100                 Bright Black (Gray)
+ * 91                  101                 Bright Red
+ * 92                  102                 Bright Green
+ * 93                  103                 Bright Yellow
+ * 94                  104                 Bright Blue
+ * 95                  105                 Bright Magenta
+ * 96                  106                 Bright Cyan
+ * 97                  107                 Bright White
+ * ---------------------------------------------------------
+ * [기타 제어]
+ * 0 : 모든 속성 초기화 (Reset)
+ * 1 : 굵게 (Bold)
+ * 4 : 밑줄 (Underline)
+ * ---------------------------------------------------------
+ * 사용법: printf("\x1b[%dm", 코드);
+ */
+
+
+
+/**
+ * 폰트(글자) 색상을 변경하는 함수
+ * @param code: 30-37 (기본), 90-97 (밝은 색)
+ 
+ * 배경 색상을 변경하는 함수
+ * @param code: 40-47 (기본), 100-107 (밝은 색)
+ */
+
+void set_color(int code) 
+{
+	printf("\x1b[%dm", code);
+}
+
 int move_cursor(int x, int y)
 {
 	printf("\033[%d;%dH", y, x);	//printf : print function
@@ -14,12 +78,27 @@ int main()
 
 	while (isRunning)
 	{
+		move_cursor(50, 10);
+		set_color(BG_COLOR_BRIGHTMAGENTA);
+		set_color(FONT_COLOR_WHITE);
+
+		printf("임서진의 첫사랑");
+
+		set_color(BG_COLOR_BLACK);
+		
+
+		move_cursor(51, 13);
+		printf("1. 게임  시작");
+
+		move_cursor(51, 15);
+		printf("2. 게임  종료");
+
 		char a = getch();
-		if (a == ' ')
+		if (a == '1')
 		{
 			x = x + 1;
 		}
-		if (a == 'q')
+		if (a == '2')
 		{
 			isRunning = 0;
 		}
@@ -27,7 +106,7 @@ int main()
 		move_cursor(x, y);
 		printf("*");
 	}
-	printf("%c", a);
+
 
 	return 0;
 }
